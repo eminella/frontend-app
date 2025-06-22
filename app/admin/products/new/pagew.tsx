f3e93d6c8fc4f555c0aa6da555cd3e303ab6ac24
Eminella <kinalikursad@gmail.com>
2025-06-22 13:17:28 +0300

    feat: /admin/products/new ürün ekleme sayfası eklendi

---
+++ app/admin/products/new/pagew.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import ProductForm from '@/components/ProductForm';
+
+export default function NewProductPage() {
+  return (
+    <div className="max-w-3xl mx-auto">
+      <h1 className="text-2xl font-bold mb-6">Yeni Ürün Ekle</h1>
+      <ProductForm />
+    </div>
+  );
+}
