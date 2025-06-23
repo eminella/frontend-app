ae8fabdfef60006740972798a8493b884f97b624
Eminella <kinalikursad@gmail.com>
2025-06-23 09:53:14 +0300

    fix: site genel arka planı beyaz yapıldı, yazılar siyah

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="tr">
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen bg-white text-black">
+
         <CartProvider>
           <Header />
 
